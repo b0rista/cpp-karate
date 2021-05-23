f2f508b687a4398062a1efe11791a58857d7b0ea
Borys Tarovyk <b.tarovik@gmail.com>
2021-05-23 15:27:58 +0300

    - add a+b assignement

---
+++ WhiteBelt/Week1/APlusB/APlusB/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  APlusB
+//
+//  Created by Boris Tarovik on 23.05.2021.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
